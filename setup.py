7a9375c188b14957dd34e7c71d145f609b64cbf6
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-10-27 23:32:50 +0200

    setup.py written

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='scripting',
+      version='0.1',
+      description='A set of shell utilities that makes working on python scripts a breeze',
+      author='Gabriele Lanaro',
+      author_email='gabriele.lanaro@gmail.com',
+      url='http://scripting.readthedocs.com',
+      packages=['scripting'],
+      classifier = '''License :: OSI Approved :: GNU General Public License (GPL)
+    Development Status :: 1 - Planning'''
+     )
